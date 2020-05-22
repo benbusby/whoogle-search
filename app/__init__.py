f1e17d81190b7dfacc5078ed4490ca3e8aa20040
Ben Busby <benbusby@protonmail.com>
2020-05-22 10:38:58 -0600

    Bumped version to v0.1.2

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__, static_folder=os.path.dirname(os.path.abspath(__file__)) + '/static')
 app.secret_key = Fernet.generate_key()
-app.config['VERSION_NUMBER'] = '0.1.1'
+app.config['VERSION_NUMBER'] = '0.1.2'
 app.config['APP_ROOT'] = os.getenv('APP_ROOT', os.path.dirname(os.path.abspath(__file__)))
 app.config['STATIC_FOLDER'] = os.getenv('STATIC_FOLDER', os.path.join(app.config['APP_ROOT'], 'static'))
 app.config['CONFIG_PATH'] = os.getenv('CONFIG_VOLUME', app.config['STATIC_FOLDER']) + '/config.json'
