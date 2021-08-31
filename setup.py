5d86326ae63a2ae5fbea6999b3a066278e09fdca
Ben Busby <noreply+git@benbusby.com>
2021-08-31 09:00:55 -0600

    Append timestamp to TestPyPI build versions
    This should allow the same "version" to be uploaded for each commit.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import os
 import setuptools
+import time
 
 long_description = open('README.md', 'r').read()
 
 requirements = list(open('requirements.txt', 'r'))
 
+optional_dev_tag = ''
+if os.getenv('DEV_BUILD'):
+    optional_dev_tag = '.dev' + str(int(time.time()))
+
 setuptools.setup(
     author='Ben Busby',
-    author_email='benbusby@protonmail.com',
+    author_email='contact@benbusby.com',
     name='whoogle-search',
-    version='0.5.4',
+    version='0.5.4' + optional_dev_tag,
     include_package_data=True,
     install_requires=requirements,
     description='Self-hosted, ad-free, privacy-respecting metasearch engine',
