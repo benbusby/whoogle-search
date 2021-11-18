92577330b6b82175174349978b23d84c92121bd4
Vansh Comar <vanshcomar1@gmail.com>
2021-11-18 12:29:58 +0530

    Correcting syntax

--- app/static/js/currency.js
+++ app/static/js/currency.js
@@ -4,7 +4,7 @@ function Convert(n1, n2, conversion_factor) {
     id2 = "cb" + n2;
 
     // getting the value of the input box that just got filled
-    var input_box = document.getElementById(id1).value
+    var input_box = document.getElementById(id1).value;
 
     // updating the other input box after conversion
     document.getElementById(id2).value = ((input_box * conversion_factor).toFixed(2));
