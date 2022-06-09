35ac5ac82fc5a6e89439582416ac37884428cab8
Ben Busby <contact@benbusby.com>
2022-06-09 16:40:49 -0600

    Fix autocomplete behavior on result page
    Similar issue to #629, but the result page uses a different script for
    handling user input, so the fix was not applied appropriately.
    
    It has been fixed for this view now.

--- app/static/js/header.js
+++ app/static/js/header.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchBar = document.getElementById("search-bar");
+    const arrowKeys = [37, 38, 39, 40];
+    let searchValue = searchBar.value;
 
-    searchBar.addEventListener("keyup", function (event) {
-        if (event.keyCode !== 13) {
-            handleUserInput(searchBar);
-        } else {
+    searchBar.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
             document.getElementById("search-form").submit();
+        } else if (searchBar.value !== searchValue && !arrowKeys.includes(event.keyCode)) {
+            searchValue = searchBar.value;
+            handleUserInput();
         }
     });
 });
