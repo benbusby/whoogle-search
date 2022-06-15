4f625fb18e61d5440da8499816ce03d6e6c5a242
jacr13 <ramos.joao@protonmail.com>
2022-06-15 19:52:35 +0200

    import get_first_link from the correct place

--- app/utils/search.py
+++ app/utils/search.py
@@ -2,8 +2,9 @@ import os
 import re
 from typing import Any
 
-from app.filter import Filter, get_first_link
+from app.filter import Filter
 from app.request import gen_query
+from app.utils.results import get_first_link
 from bs4 import BeautifulSoup as bsoup
 from cryptography.fernet import Fernet, InvalidToken
 from flask import g
