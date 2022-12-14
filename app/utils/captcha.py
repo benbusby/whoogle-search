b26aeec17323aeca9681506db0b90675708d45e8
Moist-Cat <lamatcardenas@gmail.com>
2022-12-14 11:12:20 -0500

    Tests pass

--- app/utils/captcha.py
+++ app/utils/captcha.py
@@ -1,24 +1,63 @@
+"""
+Itegration with third party CAPTCHA solving services
+"""
+# only deathbycaptcha atm but whatever
 import os
 
 from bs4 import BeautifulSoup as bs
+
 try:
     import deathbycaptcha
 except ImportError:
     deathbycaptcha = None
 
+
+class UnableToSolve(Exception):
+    """
+    The third-party service was unable to solve the CAPTCHA
+    """
+
+
 def parse_params(response):
+    """
+    Parses a page with bs4 to fetch the data needed to solve the captcha.
+    """
     params = {
-        "googlekey":"",
+        "googlekey": "",
         "data-s": "",
-        "pageurl": ""
+        "pageurl": "",
     }
-    soup = bs(response)
+    soup = bs(response.text)
+
+    recaptcha = soup.find(id="recaptcha")
+    if not recaptcha:
+        # i could save the page for debugging since this is usually
+        # hard to reproduce
+        raise AttributeError(
+            "Couldn't find the element with the CAPTCHA params"
+            "Are you sure this page contains Google's reCAPTCHA v2 with callback?"
+        )
+    params["googlekey"] = recaptcha.attrs["data-sitekey"]
+    params["data-s"] = recaptcha.attrs["data-s"]
+
+    params["pageurl"] = response.url
+
+    return params
+
 
 def solve(response):
+    """
+    Get a response with a reCAPTCHA v2 and solve it using a third-party service
+    """
     if deathbycaptcha is None:
         raise ImportError("The deathbycaptcha client is not installed")
 
     client = deathbycaptcha.HttpClient(
-        os.env.get("DBC_USER", "username"),
-        os.env.get("DBC_PASS")
+        os.environ.get("DBC_USER", "username"), os.environ.get("DBC_PASS", "password")
     )
+
+    params = parse_params(response)
+
+    token = client.decode(type=4, token_params=params)
+    if not token or token == "?":
+        raise UnableToSolve("Deathbycaptcha was unable to solve the captcha")
---
+++ test/test_captcha.py
@@ -0,0 +1,47 @@
+"""
+Test the integration with third-party CAPTCHA solving services
+"""
+
+from pathlib import Path
+from argparse import Namespace
+
+from app.utils import captcha
+
+TEST_FILES = Path(__file__).parent / "test_files"
+
+
+def test_parse():
+    """
+    Test the parsing functionality
+    """
+
+    with open(TEST_FILES / "recaptcha_v2_callback.html") as file:
+        text = file.read()
+    # primitive mock
+    response = Namespace()
+    response.url = "https://www.google.com/search?gbv=1&num=10&q=Liddell&safe=off"
+    response.text = text
+
+    res = captcha.parse_params(response)
+
+    data_s = (
+        "I_wQ5kiIMUbCdcGyC1x6zzK70nD"
+        "G9kViGr7TS6zaiWsIdZXcmQGoaxN"
+        "hiGulX8tD_xNYFXLRkLFSkxDnrkIr"
+        "5o5xSw2Sj1Z-bs5dqP2TyQFGBaTZFY"
+        "sRBy3CoDJruyranhLqWoWb3mdxvgUb"
+        "kpS7ZkRSFYFP_dg9WV4rIQxa6OUmrAt"
+        "S6JKw_UbHN8tJ4mCpz6BKYsGB_fjyD9"
+        "fuRrzmn2RK8FzsOAiLEWBc0z5Qxdltd"
+        "owqO1ugNxQdSaqM39pF73cCAqWqEama"
+        "RRa9iOOVflHptIHjo88"
+    )
+
+    expected = {
+        "googlekey": "6LfwuyUTAAAAAOAmoS0fdqijC2PbbdH4kjq62Y1b",
+        "data-s": data_s,
+        "pageurl": "https://www.google.com/search?gbv=1&num=10&q=Liddell&safe=off",
+    }
+
+    message = "Results differ\n" f"Expected: {expected}\n" f"Got: {res}"
+    assert res == expected, message
