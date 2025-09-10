b7bd56f6da82095fd4a8a84ca04086fc19183f27
Don-Swanson <32144818+Don-Swanson@users.noreply.github.com>
2025-09-10 17:50:40 -0500

    Updated version number

--- app/version.py
+++ app/version.py
@@ -4,4 +4,4 @@ optional_dev_tag = ''
 if os.getenv('DEV_BUILD'):
     optional_dev_tag = '.dev' + os.getenv('DEV_BUILD')
 
-__version__ = '0.9.3' + optional_dev_tag
+__version__ = '0.9.4' + optional_dev_tag
